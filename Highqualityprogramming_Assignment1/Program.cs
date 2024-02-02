ee5a90353bf69b0508474d561bec70b9df720d70
sushm <sushm@DESKTOP-L0OUK9L>
2024-02-02 14:00:46 -0500

    Add project files.

---
+++ Highqualityprogramming_Assignment1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
+Console.WriteLine("Choose one of the pet below:\n 1. Dog \n 2. Cat \n 3. Chipmunk");
+int userinput = Convert.ToInt32(Console.ReadLine());
+if (userinput==1)
+Console.WriteLine("You have selected Dog as your pet");
+if (userinput == 2)
+    Console.WriteLine("You have selected Cat as your pet");
+if (userinput == 3)
+    Console.WriteLine("You have selected Chipmunk as your pet");
+
+
