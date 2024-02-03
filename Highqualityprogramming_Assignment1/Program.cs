c82a0e881ee35ad267f618df99fe8bcd2ea8bfb8
Sushmitha Ranjith <Sranjith5585@conestogac.on.ca>
2024-02-02 19:05:04 -0500

    Pet Creation Success

--- Highqualityprogramming_Assignment1/Program.cs
+++ Highqualityprogramming_Assignment1/Program.cs
@@ -1,13 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection.Metadata;
+﻿Console.WriteLine("Choose one of the pet below:\n 1. Dog \n 2. Cat \n 3. Chipmunk");
+int userinput = Convert.ToInt32(Console.ReadLine());//Read User Input from console
+Console.WriteLine("User selected: " + userinput);
+if (userinput == 1)
+{
+    Console.WriteLine("You have selected Dog as your pet. What do you wanna name it?");
+}
+else if (userinput == 2)
+{
+    Console.WriteLine("You have selected Cat as your pet. What would you wanna name it?");
+}
 
-Console.WriteLine("Choose one of the pet below:\n 1. Dog \n 2. Cat \n 3. Chipmunk");
-int userinput = Convert.ToInt32(Console.ReadLine());
-if (userinput==1)
-Console.WriteLine("You have selected Dog as your pet");
-if (userinput == 2)
-    Console.WriteLine("You have selected Cat as your pet");
-if (userinput == 3)
-    Console.WriteLine("You have selected Chipmunk as your pet");
+else if (userinput == 3)
+{
+    Console.WriteLine("You have selected Chipmunk as your pet. What would you wanna name it?");
+}
+string petname = Console.ReadLine();
+Console.WriteLine(" User input :" + petname);
+Console.WriteLine("Hi " + petname + " Let me take good care of you");
 
 
