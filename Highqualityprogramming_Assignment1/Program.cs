f5476ce7b062c76a0831923e0854865eb26186b3
Sushmitha Ranjith <Sranjith5585@conestogac.on.ca>
2024-02-02 19:24:01 -0500

    Pet Status Monitoring Console Display and getting default values for pets status

--- Highqualityprogramming_Assignment1/Program.cs
+++ Highqualityprogramming_Assignment1/Program.cs
@@ -14,8 +14,16 @@ else if (userinput == 3)
 {
     Console.WriteLine("You have selected Chipmunk as your pet. What would you wanna name it?");
 }
-string petname = Console.ReadLine();
+string petname = Console.ReadLine();//Read the pet name 
 Console.WriteLine(" User input :" + petname);
 Console.WriteLine("Hi " + petname + " Let me take good care of you");
 
+//Setting default Values
 
+int Hunger = 5;
+int Happiness = 5;
+int Health = 5;
+
+Console.WriteLine("Main Menu:\n 1.Feed " + petname + "\n 2.Play with " + petname + "\n 3.Let " + petname + " Rest \n 4.Check " + petname + "'s Status \n 5.Exit");
+userinput = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("User selected: " + userinput);
